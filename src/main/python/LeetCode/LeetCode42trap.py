24f5ef9ef59e407087024671ae3c6ac7bb875f4b
540928898 <540928898@qq.com>
2020-02-17 15:25:11 +0800

    接雨水和旋转链表

--- src/main/python/LeetCode/LeetCode42trap.py
+++ src/main/python/LeetCode/LeetCode42trap.py
@@ -20,6 +20,33 @@ class Solution:
             for i in range(left,right+1):
                 res += height[i] <level
         return res
+    def trap2(self, height) :
+        '''
+        使用双指针，记录最大值
+        :param height:
+        :return:
+        '''
+        left = 0
+        right = len(height)-1
+        left_max = 0
+        right_Max = 0
+        res = 0
+        while(left < right):
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    res += left_max-height[left]
+                left += 1
+            else:
+                if height[right] >= right_Max:
+                    right_Max = height[right]
+                else:
+                    res += right_Max-height[right]
+                right -= 1
+        return res
+
+
 
 
 
